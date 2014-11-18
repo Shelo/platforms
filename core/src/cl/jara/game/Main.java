d95bcc485913dd125270272cea3e8193a6c26f40
Shelo <marjara35@gmail.com>
2014-11-18 11:04:10 -0400

    Add screen structure.

--- core/src/cl/jara/game/Main.java
+++ core/src/cl/jara/game/Main.java
@@ -1,27 +1,11 @@
 package cl.jara.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Main extends com.badlogic.gdx.Game {
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		setScreen(new Game());
 	}
 }
